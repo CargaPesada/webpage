0e56af5e55b9c2f57405623ea5c168a8c81b6477
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2019-09-10 13:51:02 -0300

    Register System done but NOT tested

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+class User {
+    constructor(email, senha, nome, cpf, tipocnh, sexo, ddn, nomepai, nomemae, dependentes, endereco) {
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.tipocnh = tipocnh;
+        this.sexo = sexo;
+        this.ddn = ddn;
+        this.nomepai = nomepai;
+        this.nomemae = nomemae;
+        this.dependentes = dependentes;
+        this.endereco = endereco;
+    }
+}
+
+export default User;
--- src/utils/firebase/FirebaseHandler.js
+++ src/utils/firebase/FirebaseHandler.js
@@ -2,32 +2,28 @@ const axios = require('axios');
 
 class FirebaseHandler {
 
-    tryToRegister = () => {
+    tryToRegister = async (user) => {
 
-        axios.post('https://carga-pesada-d933f.appspot.com/users/register', {
-            nome: "Vinícius Higa",
-            cargo: 4,
-            email: "vinicius.hh@apple.com",
-            senha: "swiftmelhorquekotlin",
-            rg: "38.733.798-x",
-            cpf: "448.892.118-xy",
-            endereco: [{ "cidade": "Campinas", "estado": "SP" }],
-            tipocnh: ["C", "D"],
-            ddn: "1997-06-10",
-            sexo: "masculino",
-            nomepai: "Eduardo Higa",
-            nomemae: "Samantha Zani",
-            dependentes: [{
-                nome: "Lucas Higa",
-                tipodependencia: "Irmão",
-                rg: "12345",
-                cpf: "67890",
-                ddn: "2000-07-10",
-                sexo: "masculino",
-                endereco: [{ "cidade": "Campinas", "estado": "SP" }]
-            }],
+        let jsonToSend = {
+            nome: user.nome,
+            cargo: 1,
+            email: user.email,
+            senha: user.senha,
+            //rg: "38.733.798-x",
+            cpf: user.cpf,
+            endereco: [{ cidade: user.cidade, estado: user.estado }],
+            tipocnh: user.tipocnh,
+            ddn: user.ddn,
+            sexo: user.sexo,
+            nomepai: user.nomepai,
+            nomemae: user.nomemae,
+            dependentes: user.dependentes,
             ocorrencias: []
-        })
+        };
+
+        console.log(jsonToSend)
+
+        await axios.post('https://carga-pesada-d933f.appspot.com/users/register', jsonToSend)
             .then((res) => {
                 console.log("Funcionou");
             })
