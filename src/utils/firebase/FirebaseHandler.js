c2d8ca380a8377f216ba8e41a46771f30f5af453
¨RodrigoCSoares¨ <soares.c.rodrigo@gmail.com>
2019-09-12 22:44:26 -0300

    Add firebase config for deployment

--- src/utils/firebase/FirebaseHandler.js
+++ src/utils/firebase/FirebaseHandler.js
@@ -1,71 +1,59 @@
 const axios = require('axios');
 
-const ENDPOINT_ADDRESS = "http://localhost:5000"
+const ENDPOINT_ADDRESS = 'https://carga-pesada-d933f.appspot.com';
 
 class FirebaseHandler {
-
-    /**
+	/**
      * Método para tentar realizar um novo registro de usuário.
      */
-    tryToRegister = async (user) => {
+	tryToRegister = async (user) => {
+		let wasSuccessful = false;
 
-        let wasSuccessful = false;
+		let jsonToSend = {
+			nome: user.nome,
+			cargo: 1,
+			email: user.email,
+			senha: user.senha,
+			//rg: "38.733.798-x",
+			cpf: user.cpf,
+			endereco: [ { cidade: user.cidade, estado: user.estado } ],
+			tipocnh: user.tipocnh,
+			ddn: user.ddn,
+			sexo: user.sexo,
+			nomepai: user.nomepai,
+			nomemae: user.nomemae,
+			dependentes: user.dependentes,
+			ocorrencias: []
+		};
 
-        let jsonToSend = {
-            nome: user.nome,
-            cargo: 1,
-            email: user.email,
-            senha: user.senha,
-            //rg: "38.733.798-x",
-            cpf: user.cpf,
-            endereco: [{ cidade: user.cidade, estado: user.estado }],
-            tipocnh: user.tipocnh,
-            ddn: user.ddn,
-            sexo: user.sexo,
-            nomepai: user.nomepai,
-            nomemae: user.nomemae,
-            dependentes: user.dependentes,
-            ocorrencias: []
-        };
+		// Acionando promisses para o endpoint
+		await axios.post(ENDPOINT_ADDRESS + '/users/register', jsonToSend).then((res) => {
+			wasSuccessful = true;
+		});
 
-        // Acionando promisses para o endpoint
-        await axios.post(ENDPOINT_ADDRESS + '/users/register', jsonToSend)
-            .then((res) => {
-                wasSuccessful = true;
-            })
+		return wasSuccessful;
+	};
 
-        return wasSuccessful;
-    }
-
-
-    /**
+	/**
      * Método para tentar realizar login.
      */
-    tryToLogin = async (email) => {
+	tryToLogin = async (email) => {
+		let dataExists = false;
 
-        let dataExists = false
+		// Acionando promisses para o endpoint
+		await axios.get(ENDPOINT_ADDRESS + '/users/' + email).then((res) => {
+			// Verificando se existe um campo "data"
+			try {
+				let jsonReceived = res['data'];
 
-        // Acionando promisses para o endpoint
-        await axios.get(ENDPOINT_ADDRESS + '/users/' + email)
-            .then((res) => {
+				if (jsonReceived['data'] != null) {
+					dataExists = true;
+				}
+			} catch (e) {}
+		});
 
-                // Verificando se existe um campo "data"
-                try {
-                    let jsonReceived = res["data"]
-
-                    if (jsonReceived["data"] != null) {
-                        dataExists = true
-                    }
-                }
-                catch (e) { }
-
-            })
-
-        return dataExists
-
-
-    }
-
+		return dataExists;
+	};
 }
 
 export default FirebaseHandler;
