610b087662c2ef6f123c370266898fd0b2596206
Vinícius Hiroshi Higa <vinnyhiga137@hotmail.com>
2019-09-18 20:38:22 -0300

    Cargo verification step changed

--- src/components/views/user/register/RegisterView.js
+++ src/components/views/user/register/RegisterView.js
@@ -48,116 +48,115 @@ class RegisterView extends React.Component {
      */
     registerNewUser = async () => {
 
-        if (this.state.newUserCategory != -1) {
+        let errorMessages = "";
 
-            let errorMessages = "";
+        let email = document.getElementById('email').value;
+        let senha = document.getElementById('senha').value;
+        let nome = document.getElementById('nome').value;
+        let cpf = document.getElementById('cpf').value;
+        let tipocnh = "";
+        if (document.getElementById('tipocnh') != null) {
+            tipocnh = document.getElementById('tipocnh').value;
+        }
+        let sexo = this.state.userSexRadioValue;
+        let ddn = document.getElementById('ddn').value;
+        let nomepai = document.getElementById('nomepai').value;
+        let nomemae = document.getElementById('nomemae').value;
+        let dependentes = this.state.areThereDependents;
+        let endereco = {
+            cep: document.getElementById('cep').value,
+            cidade: document.getElementById('cidade').value,
+            estado: document.getElementById('estado').value,
+            rua: document.getElementById('rua').value,
+            complemento: document.getElementById('complemento').value,
+            numero: document.getElementById('numero').value,
+            bairro: document.getElementById('bairro').value
+        };
+        let nivelacesso = this.state.newUserCategory;
 
-            let email = document.getElementById('email').value;
-            let senha = document.getElementById('senha').value;
-            let nome = document.getElementById('nome').value;
-            let cpf = document.getElementById('cpf').value;
-            let tipocnh = "";
-            if (document.getElementById('tipocnh') != null) {
-                tipocnh = document.getElementById('tipocnh').value;
+
+        // Validando o campo de e-mail
+        if (email === "" || /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email) == false) {
+            errorMessages += "\n* E-Mail não está no padrão";
+        }
+
+        // Validando o campo de senha
+        if (senha.length < 6) {
+            errorMessages += "\n* Senha menor que 6 caracteres";
+        }
+
+        // Validando o campo nome
+        if (nome.length === 0) {
+            errorMessages += "\n* Nome não preenchido";
+        }
+
+        // Validando o campo CPF
+        if (cpf.length === 0) {
+            errorMessages += "\n* CPF não preenchido";
+        }
+
+        // Validando o campo CNH
+        if (this.state.newUserCategory == 0) {
+            if (tipocnh.length === 0) {
+                errorMessages += "\n* CNH não preenchido";
             }
-            let sexo = this.state.userSexRadioValue;
-            let ddn = document.getElementById('ddn').value;
-            let nomepai = document.getElementById('nomepai').value;
-            let nomemae = document.getElementById('nomemae').value;
-            let dependentes = this.state.areThereDependents;
-            let endereco = {
-                cep: document.getElementById('cep').value,
-                cidade: document.getElementById('cidade').value,
-                estado: document.getElementById('estado').value,
-                rua: document.getElementById('rua').value,
-                complemento: document.getElementById('complemento').value,
-                numero: document.getElementById('numero').value,
-                bairro: document.getElementById('bairro').value
-            };
-            let nivelacesso = this.state.newUserCategory;
-
-
-            // Validando o campo de e-mail
-            if (email === "" || /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email) == false) {
-                errorMessages += "\n* E-Mail não está no padrão";
+            else {
+                // valida_cnh(tipocnh);
+                // <script>
+                //     function valida_cnh(tipocnh) {
+                //         var dv = substring (tipocnh, 9, 2);
+                //         var dvr = ( (2 * substring (tipocnh, 1, 1) ) + (3 * substring (tipocnh, 2, 1) ) + (4 * substring (tipocnh, 3, 1) )
+                //                   + (5 * substring (tipocnh, 4, 1) ) + (6 * substring (tipocnh, 5, 1) ) + (7 * substring (tipocnh, 6, 1) )
+                //                   + (8 * substring (tipocnh, 7, 1) ) + (9 * substring (tipocnh, 8, 1) ) )
+                //                   % (11);
+                //         if (dv == dvr)
+                //             return true;
+                //         else
+                //             return false;
+                //     }
+                // </script>
             }
+        }
 
-            // Validando o campo de senha
-            if (senha.length < 6) {
-                errorMessages += "\n* Senha menor que 6 caracteres";
-            }
+        // Validando o campo DDN
+        if (ddn.length === 0) {
+            errorMessages += "\n* DDN não preenchido";
+        }
 
-            // Validando o campo nome
-            if (nome.length === 0) {
-                errorMessages += "\n* Nome não preenchido";
-            }
+        // Validando o campo nome da mãe
+        if (nomemae.length === 0) {
+            errorMessages += "\n* Nome da mãe não preenchido";
+        }
 
-            // Validando o campo CPF
-            if (cpf.length === 0) {
-                errorMessages += "\n* CPF não preenchido";
-            }
+        // Validando os campos de endereço
+        if (endereco['cep'].length < 9) { // Aceitando 2 casos: 13085-000 ou 13085000
+            errorMessages += "\n* CEP inválido";
+        }
 
-            // Validando o campo CNH
-            if (this.state.newUserCategory == 0) {
-                if (tipocnh.length === 0) {
-                    errorMessages += "\n* CNH não preenchido";
-                }
-                else {
-                    // valida_cnh(tipocnh);
-                    // <script>
-                    //     function valida_cnh(tipocnh) {
-                    //         var dv = substring (tipocnh, 9, 2);
-                    //         var dvr = ( (2 * substring (tipocnh, 1, 1) ) + (3 * substring (tipocnh, 2, 1) ) + (4 * substring (tipocnh, 3, 1) )
-                    //                   + (5 * substring (tipocnh, 4, 1) ) + (6 * substring (tipocnh, 5, 1) ) + (7 * substring (tipocnh, 6, 1) )
-                    //                   + (8 * substring (tipocnh, 7, 1) ) + (9 * substring (tipocnh, 8, 1) ) )
-                    //                   % (11);
-                    //         if (dv == dvr)
-                    //             return true;
-                    //         else
-                    //             return false;
-                    //     }
-                    // </script>
-                }
-            }
+        if (endereco['cidade'].length === 0) {
+            errorMessages += "\n* Cidade não preenchida";
+        }
 
-            // Validando o campo DDN
-            if (ddn.length === 0) {
-                errorMessages += "\n* DDN não preenchido";
-            }
+        if (endereco['estado'].length === 0) {
+            errorMessages += "\n* Estado não preenchido";
+        }
 
-            // Validando o campo nome da mãe
-            if (nomemae.length === 0) {
-                errorMessages += "\n* Nome da mãe não preenchido";
-            }
+        if (endereco['rua'].length === 0) {
+            errorMessages += "\n* Rua não preenchida";
+        }
 
-            // Validando os campos de endereço
-            if (endereco['cep'].length < 9) { // Aceitando 2 casos: 13085-000 ou 13085000
-                errorMessages += "\n* CEP inválido";
-            }
+        if (endereco['numero'].length === 0) {
+            errorMessages += "\n* Número não preenchido";
+        }
 
-            if (endereco['cidade'].length === 0) {
-                errorMessages += "\n* Cidade não preenchida";
-            }
+        if (endereco['bairro'].length === 0) {
+            errorMessages += "\n* Bairro não preenchido";
+        }
 
-            if (endereco['estado'].length === 0) {
-                errorMessages += "\n* Estado não preenchido";
-            }
+        // Verificando se existe mensagens de erros a serem exibidas...
+        if (errorMessages === "") {
 
-            if (endereco['rua'].length === 0) {
-                errorMessages += "\n* Rua não preenchida";
-            }
-
-            if (endereco['numero'].length === 0) {
-                errorMessages += "\n* Número não preenchido";
-            }
-
-            if (endereco['bairro'].length === 0) {
-                errorMessages += "\n* Bairro não preenchido";
-            }
-
-            // Verificando se existe mensagens de erros a serem exibidas...
-            if (errorMessages === "") {
-
+            if (this.state.newUserCategory != -1) {
                 let newUser = new User(email, senha, nome, cpf, tipocnh, sexo, ddn, nomepai, nomemae,
                     dependentes, endereco, nivelacesso);
 
@@ -172,11 +171,11 @@ class RegisterView extends React.Component {
                 });
             }
             else {
-                alert('Os seguintes campos estão incorretos:\n'.concat(errorMessages));
+                alert('Selecione um cargo primeiro!');
             }
         }
         else {
-            alert('Selecione um cargo primeiro!');
+            alert('Os seguintes campos estão incorretos:\n'.concat(errorMessages));
         }
     };
 
